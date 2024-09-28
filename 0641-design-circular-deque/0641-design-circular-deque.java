50d7d9f188c32a0704ec6299d5a57113580ff4d2
SAHIL KOSHRIYA <134610143+SAHILKOSHRIYA@users.noreply.github.com>
2024-09-27 18:31:50 -0700

    Time: 4 ms (100.00%), Space: 44.7 MB (46.30%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.java
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -1,3 +1,87 @@
+// class MyCircularDeque {
+//     private int[] deque;
+//     private int front;
+//     private int rear;
+//     private int size;
+//     private int capacity;
+
+//     // Constructor to initialize the deque with a maximum size of k.
+//     public MyCircularDeque(int k) {
+//         deque = new int[k];
+//         front = 0;
+//         rear = 0;
+//         size = 0;
+//         capacity = k;
+//     }
+
+//     // Insert an item at the front of the deque.
+//     public boolean insertFront(int value) {
+//         if (isFull()) {
+//             return false;
+//         }
+//         front = (front - 1 + capacity) % capacity;
+//         deque[front] = value;
+//         size++;
+//         return true;
+//     }
+
+//     // Insert an item at the rear of the deque.
+//     public boolean insertLast(int value) {
+//         if (isFull()) {
+//             return false;
+//         }
+//         deque[rear] = value;
+//         rear = (rear + 1) % capacity;
+//         size++;
+//         return true;
+//     }
+
+//     // Delete an item from the front of the deque.
+//     public boolean deleteFront() {
+//         if (isEmpty()) {
+//             return false;
+//         }
+//         front = (front + 1) % capacity;
+//         size--;
+//         return true;
+//     }
+
+//     // Delete an item from the rear of the deque.
+//     public boolean deleteLast() {
+//         if (isEmpty()) {
+//             return false;
+//         }
+//         rear = (rear - 1 + capacity) % capacity;
+//         size--;
+//         return true;
+//     }
+
+//     // Get the front item from the deque.
+//     public int getFront() {
+//         if (isEmpty()) {
+//             return -1;
+//         }
+//         return deque[front];
+//     }
+
+//     // Get the rear item from the deque.
+//     public int getRear() {
+//         if (isEmpty()) {
+//             return -1;
+//         }
+//         return deque[(rear - 1 + capacity) % capacity];
+//     }
+
+//     // Check if the deque is empty.
+//     public boolean isEmpty() {
+//         return size == 0;
+//     }
+
+//     // Check if the deque is full.
+//     public boolean isFull() {
+//         return size == capacity;
+//     }
+// }
 class MyCircularDeque {
     private int[] deque;
     private int front;
@@ -5,16 +89,13 @@ class MyCircularDeque {
     private int size;
     private int capacity;
 
-    // Constructor to initialize the deque with a maximum size of k.
     public MyCircularDeque(int k) {
-        deque = new int[k];
+        capacity = k + 1;
+        deque = new int[capacity];
         front = 0;
         rear = 0;
-        size = 0;
-        capacity = k;
     }
 
-    // Insert an item at the front of the deque.
     public boolean insertFront(int value) {
         if (isFull()) {
             return false;
@@ -25,7 +106,6 @@ class MyCircularDeque {
         return true;
     }
 
-    // Insert an item at the rear of the deque.
     public boolean insertLast(int value) {
         if (isFull()) {
             return false;
@@ -36,7 +116,6 @@ class MyCircularDeque {
         return true;
     }
 
-    // Delete an item from the front of the deque.
     public boolean deleteFront() {
         if (isEmpty()) {
             return false;
@@ -46,7 +125,6 @@ class MyCircularDeque {
         return true;
     }
 
-    // Delete an item from the rear of the deque.
     public boolean deleteLast() {
         if (isEmpty()) {
             return false;
@@ -56,7 +134,6 @@ class MyCircularDeque {
         return true;
     }
 
-    // Get the front item from the deque.
     public int getFront() {
         if (isEmpty()) {
             return -1;
@@ -64,7 +141,6 @@ class MyCircularDeque {
         return deque[front];
     }
 
-    // Get the rear item from the deque.
     public int getRear() {
         if (isEmpty()) {
             return -1;
@@ -72,13 +148,12 @@ class MyCircularDeque {
         return deque[(rear - 1 + capacity) % capacity];
     }
 
-    // Check if the deque is empty.
     public boolean isEmpty() {
-        return size == 0;
+        return front == rear;
     }
 
-    // Check if the deque is full.
     public boolean isFull() {
-        return size == capacity;
+        return (rear + 1) % capacity == front;
     }
 }
+
